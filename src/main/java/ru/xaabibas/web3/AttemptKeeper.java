0d4462683d148d038028acc18d08f841250748f6
Xaabibas <tumuyhabirov@yandex.ru>
2025-11-02 15:47:42 +0300

    table schedule

--- src/main/java/ru/xaabibas/web3/AttemptKeeper.java
+++ src/main/java/ru/xaabibas/web3/AttemptKeeper.java
@@ -2,14 +2,16 @@ package ru.xaabibas.web3;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Named;
+import lombok.Getter;
 
 import java.util.LinkedList;
 import java.util.List;
 
 @Named("keeper")
 @ApplicationScoped
+@Getter
 public class AttemptKeeper {
-    List<Attempt> attempts = new LinkedList<>();
+    private List<Attempt> attempts = new LinkedList<>();
 
     public void add(Attempt attempt) {
         attempts.add(attempt);
